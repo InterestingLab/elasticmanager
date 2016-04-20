33a7b9864fa3ed3f72ae20fca76e35c67fc6f54b
garyelephant <garygaowork@gmail.com>
2016-04-20 17:21:29 +0800

    add models to admin site

--- indices/admin.py
+++ indices/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Alias, Close, Create, Delete, IndexSet, Mappings, Optimize, Settings, Snapshot
+
+admin.site.register(Alias)
+admin.site.register(Close)
+admin.site.register(Create)
+admin.site.register(Delete)
+admin.site.register(IndexSet)
+admin.site.register(Mappings)
+admin.site.register(Optimize)
+admin.site.register(Settings)
+admin.site.register(Snapshot)
